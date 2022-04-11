3ce586f03c301ef25c98ec0e84c7fc32ad7317c8
hind-hb <hind.hbashenh@gmail.com>
2022-04-11 14:20:06 +0300

    add q

--- api/query.py
+++ api/query.py
@@ -1,27 +1,30 @@
+from ast import parse
 from http.server import BaseHTTPRequestHandler
-from urllib import parse
-import requests
+from platform import platform
+from urllib.parse import urlparse
+import platform
+
 
 class handler(BaseHTTPRequestHandler):
 
   def do_GET(self):
-    url_path = self.path
-    url_components = parse.urlsplit(url_path)
-    query_string_list = parse.parse_qsl(url_components.query)
-    
+    s = self.path
+    url = parse.urlsplit(s)
+    query_string_list = parse.parse_qsl(url.query)
+    dic = dict(query_string_list)
 
- 
-    url = 'https://api.thedogapi.com/'
-    r = requests.get(url)
-    data = r.json()
-    
-    message = str(data)        
-  
+    name = dic.get('name')
+
+    if name:
+      message = f"Hi {name}"
+    else:
+      message = f"Hi stranger"
+
+      message += f"\nGreetings from python version {platform.python_version()}"
 
     self.send_response(200)
     self.send_header('Content-type', 'text/plain')
     self.end_headers()
 
     self.wfile.write(message.encode())
-
     return
