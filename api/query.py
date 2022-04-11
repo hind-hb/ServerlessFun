85dcbc420ea0a5ffbd59ecb19cab50d28e080f63
hind-hb <hind.hbashenh@gmail.com>
2022-04-11 14:33:14 +0300

    start

--- api/query.py
+++ api/query.py
@@ -1,13 +1,29 @@
 from http.server import BaseHTTPRequestHandler
-
+from urllib import parse
+import requests
+import random
+from random import randint
 
 class handler(BaseHTTPRequestHandler):
 
   def do_GET(self):
-    #  request is successful and you will get your response
+    url_path = self.path
+    url_components = parse.urlsplit(url_path)
+    query_string_list = parse.parse_qsl(url_components.query)
+    dic = dict(query_string_list)
+    
+    # Generate a way to build a random character
+    random_person = str(randint(0, 82)) 
+    url = 'https://swapi.dev/api/'
+    r = requests.get(url + "/people/" + random_person)
+    data = r.json()
+
+    message = str(data)        
+
     self.send_response(200)
     self.send_header('Content-type', 'text/plain')
     self.end_headers()
-    message = "hello "
+
     self.wfile.write(message.encode())
+
     return
