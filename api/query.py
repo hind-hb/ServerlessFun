d7501ec134ddc4b827e83e446f1cebbc23a60811
hind-hb <hind.hbashenh@gmail.com>
2022-04-11 14:40:26 +0300

    add ne

--- api/query.py
+++ api/query.py
@@ -1,8 +1,6 @@
 from http.server import BaseHTTPRequestHandler
 from urllib import parse
 import requests
-import random
-from random import randint
 
 class handler(BaseHTTPRequestHandler):
 
@@ -11,19 +9,24 @@ class handler(BaseHTTPRequestHandler):
     url_components = parse.urlsplit(url_path)
     query_string_list = parse.parse_qsl(url_components.query)
     dic = dict(query_string_list)
+
+
     
-    # Generate a way to build a random character
-    random_person = str(randint(0, 82)) 
-    url = 'https://swapi.dev/api/'
-    r = requests.get(url + "/people/" + random_person)
+    url = 'https://api.zippopotam.us/us/'
+    print(dic)
+    r = requests.get(url + dic['post_code'])
+    
     data = r.json()
 
-    message = str(data)        
+    print(data)
+ 
+    message = f"State: {data['places'][0]['state']} City: {data['places'][0]['place name']}"       
+    print(message)
+
 
     self.send_response(200)
     self.send_header('Content-type', 'text/plain')
     self.end_headers()
-
     self.wfile.write(message.encode())
 
     return
