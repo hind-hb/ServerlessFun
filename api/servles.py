340fd4f7922b51c08c98c4252a245ceed6cb4570
hind-hb <hind.hbashenh@gmail.com>
2022-04-11 13:01:00 +0300

    add

---
+++ api/servles.py
@@ -0,0 +1,27 @@
+from http.server import BaseHTTPRequestHandler
+from urllib import parse
+import requests
+
+class handler(BaseHTTPRequestHandler):
+
+  def do_GET(self):
+    url_path = self.path
+    url_components = parse.urlsplit(url_path)
+    query_string_list = parse.parse_qsl(url_components.query)
+    
+
+ 
+    url = 'https://api.thedogapi.com/'
+    r = requests.get(url)
+    data = r.json()
+    
+    message = str(data)        
+    
+
+    self.send_response(200)
+    self.send_header('Content-type', 'text/plain')
+    self.end_headers()
+
+    self.wfile.write(message.encode())
+
+    return
