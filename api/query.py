7aea67457146d0afa09603ba54397f20b5dcb7c1
hind-hb <hind.hbashenh@gmail.com>
2022-04-11 14:26:20 +0300

    add

--- api/query.py
+++ api/query.py
@@ -1,30 +1,13 @@
-from ast import parse
 from http.server import BaseHTTPRequestHandler
-from platform import platform
-from urllib.parse import urlparse
-import platform
 
 
 class handler(BaseHTTPRequestHandler):
 
   def do_GET(self):
-    s = self.path
-    url = parse.urlsplit(s)
-    query_string_list = parse.parse_qsl(url.query)
-    dic = dict(query_string_list)
-
-    name = dic.get('name')
-
-    if name:
-      message = f"Hi {name}"
-    else:
-      message = f"Hi stranger"
-
-      message += f"\nGreetings from python version {platform.python_version()}"
-
+    #  request is successful and you will get your response
     self.send_response(200)
     self.send_header('Content-type', 'text/plain')
     self.end_headers()
-
+    message = "hello "
     self.wfile.write(message.encode())
     return
